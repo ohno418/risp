68463f1cf3e8332267ca8e734d0582e7897a28d3
ohno418 <yutaro.ono.418@gmail.com>
2022-02-26 11:22:17 +0900

    mal step 0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::io::prelude::*;
+use std::io;
+
+mod mal {
+    pub fn repl(input: &str) -> &str {
+        print(eval(read(input)))
+    }
+
+    fn read(input: &str) -> &str {
+        input
+    }
+
+    fn eval(input: &str) -> &str {
+        input
+    }
+
+    fn print(result: &str) -> &str {
+        result
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("user> ");
+        io::stdout().flush().unwrap();
+
+        let mut input: String = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        // println!("(debug) input: {}", input);
+
+        print!("{}", mal::repl(&input));
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn repl_returns_as_is() {
+        let input = "asis";
+        assert_eq!(crate::mal::repl(input), "asis");
+    }
 }
