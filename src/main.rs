f270576f79fd2c23c886afe9b91f06e41002ba0e
ohno418 <yutaro.ono.418@gmail.com>
2022-03-19 12:12:59 +0900

    chore

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,39 @@
+mod evaluator;
 mod lexer;
 mod parser;
 mod reader;
-mod evaluator;
 
-mod risp {
-    use crate::lexer::tokenize;
-    use crate::parser::parse;
-    use crate::reader::{read_input, ReadError};
-    use crate::evaluator::eval;
-    use std::process;
-
-    pub fn repl() {
-        loop {
-            let input = match read_input() {
-                Ok(input) => input.trim().to_string(),
-                Err(ReadError::CtrlD) => {
-                    println!("\nexit");
-                    process::exit(0);
-                }
-                Err(ReadError::EmptyInput) => continue,
-            };
-
-            let tokens = tokenize(&input);
-            let node = match parse(&tokens) {
-                Some(node) => node,
-                None => {
-                    println!("Cannot parse: {}", &input);
-                    continue;
-                }
-            };
-            let result = eval(&node);
-            println!("{}", result);
-        }
-    }
-}
-
-use crate::risp::repl;
+use crate::evaluator::eval;
+use crate::lexer::tokenize;
+use crate::parser::parse;
+use crate::reader::{read_input, ReadError};
+use std::process;
 
 fn main() {
-    repl();
+    loop {
+        let input = match read_input() {
+            Ok(input) => input.trim().to_string(),
+            Err(ReadError::CtrlD) => {
+                println!("\nexit");
+                process::exit(0);
+            }
+            Err(ReadError::EmptyInput) => continue,
+        };
+
+        let tokens = tokenize(&input);
+        let node = match parse(&tokens) {
+            Some(node) => node,
+            None => {
+                println!("Cannot parse: {}", &input);
+                continue;
+            }
+        };
+        match eval(&node) {
+            Some(result) => println!("{}", result),
+            None => {
+                println!("Cannot evaluate: {}", input);
+                continue;
+            }
+        }
+    }
 }
