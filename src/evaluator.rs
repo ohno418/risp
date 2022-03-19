785ca8510af956ee8e980f6cdd05c3dea4b3a504
ohno418 <yutaro.ono.418@gmail.com>
2022-03-19 13:01:32 +0900

    Add more test cases for the eval function

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -55,10 +55,26 @@ mod tests {
         let actual = eval(&ast).unwrap();
         let expected = 35;
         assert_eq!(actual, expected);
+
+        let ast = List(vec![Sym("-".to_string()), Int(43), Int(32)]);
+        let actual = eval(&ast).unwrap();
+        let expected = 11;
+        assert_eq!(actual, expected);
+
+        let ast = List(vec![Sym("*".to_string()), Int(21), Int(2)]);
+        let actual = eval(&ast).unwrap();
+        let expected = 42;
+        assert_eq!(actual, expected);
+
+        let ast = List(vec![Sym("/".to_string()), Int(21), Int(3)]);
+        let actual = eval(&ast).unwrap();
+        let expected = 7;
+        assert_eq!(actual, expected);
     }
 
     #[test]
     fn eval_nested_arithmetic_list() {
+        // (* 2 (+ 12 23))
         let ast = List(vec![
             Sym("*".to_string()),
             Int(2),
@@ -66,6 +82,20 @@ mod tests {
         ]);
         let actual = eval(&ast).unwrap();
         let expected = 70;
+        assert_eq!(actual, expected);
+
+        // (* (+ 1 2) (- (/ 21 3) 3))
+        let ast = List(vec![
+            Sym("*".to_string()),
+            List(vec![Sym("+".to_string()), Int(1), Int(2)]),
+            List(vec![
+                 Sym("-".to_string()),
+                 List(vec![Sym("/".to_string()), Int(21), Int(3)]),
+                 Int(3),
+            ]),
+        ]);
+        let actual = eval(&ast).unwrap();
+        let expected = 12;
         assert_eq!(actual, expected);
     }
 }
