46fe5e10a2d149d774bacdbb8bddc094c1fbde9e
ohno418 <yutaro.ono.418@gmail.com>
2022-03-19 13:05:27 +0900

    chore

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -51,21 +51,25 @@ mod tests {
 
     #[test]
     fn eval_arithmetic_list() {
+        // (+ 12 23)
         let ast = List(vec![Sym("+".to_string()), Int(12), Int(23)]);
         let actual = eval(&ast).unwrap();
         let expected = 35;
         assert_eq!(actual, expected);
 
+        // (- 42 32)
         let ast = List(vec![Sym("-".to_string()), Int(43), Int(32)]);
         let actual = eval(&ast).unwrap();
         let expected = 11;
         assert_eq!(actual, expected);
 
+        // (* 21 2)
         let ast = List(vec![Sym("*".to_string()), Int(21), Int(2)]);
         let actual = eval(&ast).unwrap();
         let expected = 42;
         assert_eq!(actual, expected);
 
+        // (/ 21 3)
         let ast = List(vec![Sym("/".to_string()), Int(21), Int(3)]);
         let actual = eval(&ast).unwrap();
         let expected = 7;
